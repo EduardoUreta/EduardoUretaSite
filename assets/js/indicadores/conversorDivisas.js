c8fbdd5ae858fc7c57658ea365db785ff9861612
Eduardo Ureta <eduardo.uretaca@gmail.com>
2024-10-06 12:39:16 -0300

    se finaliza conversor de divisas con estilos

--- assets/js/indicadores/conversorDivisas.js
+++ assets/js/indicadores/conversorDivisas.js
@@ -22,13 +22,13 @@ const conversorDivisas = () => {
     botonConvertir.addEventListener('click', (e) => {
         if (valorAConvertir.value == ''){
             resultadoConversion.innerHTML = `
-                <h4 class="text-warning m-0">
+                <h4 class="m-0 text-center" style="color: rgb(12 12 54)">
                     Ingresa un valor
                 </h4>
             `;
         } else if (divisaUno.value == divisaDos.value) {
             resultadoConversion.innerHTML = `
-                <h4 class="text-warning">
+                <h4 class="m-0" style="color: rgb(12 12 54)">
                     Elige otra moneda para convertir
                 </h4>
             `;
@@ -39,7 +39,7 @@ const conversorDivisas = () => {
                     if(divisaDos.value === 'CPL'){
                         const valorActualDivisaUno = data[0].valor;
                         const calculo = valorAConvertir.value * valorActualDivisaUno;
-                        resultadoConversion.innerHTML = `<h4 class="text-warning">$${calculo} ${divisaDos.value}</h4>`;
+                        resultadoConversion.innerHTML = `<h4 class="m-0" style="color: rgb(12 12 54)">$${calculo} ${divisaDos.value}</h4>`;
                     } else {
                         const valorActualDivisaUno = data[0].valor;
                         const obtenerDatosDivisaDos = indicadoresApi(divisaDos.value);
@@ -48,7 +48,7 @@ const conversorDivisas = () => {
                                 const valorActualDivisaDos = data[0].valor;
                                 const calculo = valorAConvertir.value * valorActualDivisaUno / valorActualDivisaDos;
                                 resultadoConversion.innerHTML = `
-                                    <h4 class="text-warning">
+                                    <h4 class="">
                                         $${(calculo % 1 === 0) ? calculo : calculo.toFixed(3)} ${divisaDos.value}
                                     </h4>
                                 `;
@@ -68,7 +68,7 @@ const conversorDivisas = () => {
                     const valorActualDivisaDos = data[0].valor;
                     const calculo = valorAConvertir.value / valorActualDivisaDos;
                     resultadoConversion.innerHTML = `
-                        <h4 class="text-warning">
+                        <h4 class="m-0" style="color: rgb(12 12 54)">
                             $${(calculo % 1 === 0) ? calculo : calculo.toFixed(3)} ${divisaDos.value}
                         </h4>
                     `;
